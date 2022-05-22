2c2bdb0f11425a0480b75ab6b90b5f8c9d60aecc
Nicolas Fernandez <nico.06.89crc@gmail.com>
2022-05-22 10:51:34 -0300

    Moving RackSessionFix from application to api controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,6 @@
 class ApiController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ApiResponse
+  include RackSessionFix
   before_action :authenticate_user!
 end
